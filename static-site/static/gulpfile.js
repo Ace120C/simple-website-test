b148355c7b882d34f68ea959a52d3a38b21718dc
Ace120C <145904614+Ace120C@users.noreply.github.com>
2024-11-10 22:34:05 +0100

    updated the compiler

--- static-site/static/gulpfile.js
+++ static-site/static/gulpfile.js
@@ -1,15 +1,16 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
-function buildStyles() {
-  return src('sass/**/*.scss')
-  .pipe(sass())
-  .pipe(dest('css'))
+function buildStyles(done) {
+  src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('css'));
+  done();
 }
 
 function watchTask() {
- watch(['sass/**/*.scss'], buildStyles) 
+ watch(['sass/**/*.scss'], buildStyles); 
 }
 
 
-exports.default = series(buildStyles, watchTask)
+exports.default = series(buildStyles, watchTask);
